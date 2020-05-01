29be8827e61331132bbf10b7a5a8470921dae15a
Sergio Robles <sroblesc@gmail.com>
2020-05-01 15:47:22 +0100

    Saved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +7,31 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+
+  get '/' do 
+    "This is a list for recipes." 
+  end 
+
+  get '/recipes/new' do 
+    @recipe = Recipe.new
+    erb :new
+  end
+
+  get 'recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  post 'recipes/new' do 
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    Recipe.save
+    redirect to "/recipes/#{recipe.id}"
+  end
+
 
 end
